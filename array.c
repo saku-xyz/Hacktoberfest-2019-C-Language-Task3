c9759bfb0e7840f46d3c18f0d9774580f7192537
SHandapangoda <hsithum98@yahoo.com>
2019-10-23 22:38:27 +0530

    Insert values to an array

--- array.c
+++ array.c
@@ -2,5 +2,15 @@
 
 void  main()
 {
-    
+    int arr[10];
+    int i;
+    printf("Input 10 numbers of elements in array \n");
+    for (i=0;i<10;i++){
+        printf("element %d:",i);
+        scanf("%d",&arr[i]);
+    }
+printf("Elements in array are ");
+for(i=0;i<10;i++){
+    printf("%d ",arr[i--]);
+}
 }
