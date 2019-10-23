f24d85a42f313e349c5479e17982ce05870784c9
ppubudu4 <ppubudu4@gmail.com>
2019-10-24 00:00:48 +0530

    task 1 complete

--- array.c
+++ array.c
@@ -1,24 +1,37 @@
-#include <stdio.h>  
-  
-void  main()  
-{  
-    int arr[10]; 
-    int i;  
-       printf("\n\nRead Elements in array: \n");
-       printf("-----------------------------------------\n");	
-  
-    printf("Input 10 Elements into array : \n");  
-    for(i=1; i<=10; i++)  
-    {  
-	    printf("element - %d : ",i);
-        scanf("%d", &arr[i]);  
-    }  
-  
-    printf("\nElements in array are: ");  
-    for(i=0; i<10; i++)  
-    {  
-        printf("%d  ", arr[i]);  
-    } 
-    printf("\n");
-    printf("\n");
+#include <stdio.h>
+
+int i = 0;
+int get_array_elements(int *numbers);
+void print_numbers(int *n);
+void get_array_elements2(int *numbers);
+
+int main()
+{
+int numbers[20];
+get_array_elements(numbers);
+
+print_numbers(numbers);
+
+getch();
+return 0;
+}
+int get_array_elements(int *numbers)
+{
+printf("Input 10 elements in the array: ");
+printf("\n");
+
+for (i = 0; i < 10; i++)
+{
+printf("Element - %d : ", i+1);
+scanf("%d", &numbers[i]);
+}
+}
+void print_numbers(int *n)
+{
+printf("\nElements in array are: ");
+for (i = 0; i < 10; i++)
+{
+printf("%d ", n[i]);
+}
+}
 }
