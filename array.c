5e128f09af55bad7e6786c3466b2ccb97f913a2c
Dilmi <dskottachchi@outlook.com>
2019-10-24 12:17:41 +0530

    Adding elements to the array and displaying

--- array.c
+++ array.c
@@ -4,19 +4,20 @@ void  main()
     int arr[50]; 
     int i;  
        printf("\n Reading elements in an array: \n");
-       printf("******************************************\n");	
+       printf("**********************************\n");	
   
-    printf("Input 50 elements into the array : \n");  
-    for(i=1; i<=50; i++)  
+    printf("Input 100 elements into the array : \n");  
+    for(i=1; i<=100; i++)  
     {  
 	    printf("element - %d : ",i);
         scanf("%d", &arr[i]);  
     }  
   
-    printf("\nBelow are the elements stored in the array: ");  
-    for(i=0; i<50; i++)  
+    printf("\nElements that are stored in an array: ");  
+    for(i=0; i<100; i++)  
     {  
         printf("%d  ", arr[i]);  
     } 
+    printf("\n");
     printf("\n");
 }
