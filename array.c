ddffb4a6b37183c63e65c67c88c71c3829b8f861
Dilmi <dskottachchi@outlook.com>
2019-10-24 11:31:30 +0530

    Reading elements in an array

--- array.c
+++ array.c
@@ -1,24 +1,22 @@
 #include <stdio.h>  
-  
 void  main()  
 {  
-    int arr[10]; 
+    int arr[50]; 
     int i;  
-       printf("\n\nRead Elements in array: \n");
-       printf("-----------------------------------------\n");	
+       printf("\n Reading elements in an array: \n");
+       printf("******************************************\n");	
   
-    printf("Input 10 Elements into array : \n");  
-    for(i=1; i<=10; i++)  
+    printf("Input 50 elements into the array : \n");  
+    for(i=1; i<=50; i++)  
     {  
 	    printf("element - %d : ",i);
         scanf("%d", &arr[i]);  
     }  
   
-    printf("\nElements in array are: ");  
-    for(i=0; i<10; i++)  
+    printf("\nBelow are the elements stored in the array: ");  
+    for(i=0; i<50; i++)  
     {  
         printf("%d  ", arr[i]);  
     } 
-    printf("\n");
     printf("\n");
 }
